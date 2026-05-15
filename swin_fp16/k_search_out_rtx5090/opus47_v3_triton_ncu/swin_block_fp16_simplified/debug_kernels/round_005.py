debe095ec3f5152e44deee05fd1b34bd199f88d9
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2026-05-15 08:49:27 +0200

    tmp

---
+++ swin_fp16/k_search_out_rtx5090/opus47_v3_triton_ncu/swin_block_fp16_simplified/debug_kernels/round_005.py
@@ -0,0 +1,238 @@
+import torch
+from torch import nn, Tensor
+import triton
+import triton.language as tl
+
+
+def _make_relative_position_bias(window_size, num_heads, dtype):
+    Wh, Ww = window_size
+    table = nn.init.trunc_normal_(
+        torch.empty((2 * Wh - 1) * (2 * Ww - 1), num_heads, dtype=dtype),
+        std=0.02,
+    )
+    coords = torch.stack(torch.meshgrid(
+        torch.arange(Wh), torch.arange(Ww), indexing="ij"))
+    coords = coords.flatten(1)
+    rel = coords[:, :, None] - coords[:, None, :]
+    rel = rel.permute(1, 2, 0).contiguous()
+    rel[..., 0] += Wh - 1
+    rel[..., 1] += Ww - 1
+    rel[..., 0] *= 2 * Ww - 1
+    idx = rel.sum(-1).flatten()
+    bias = table[idx].view(Wh * Ww, Wh * Ww, num_heads).permute(2, 0, 1)
+    return bias.unsqueeze(0).contiguous()
+
+
+@triton.jit
+def _swin_block_kernel(
+    X_ptr, Y_ptr,
+    ln1_w_ptr, ln1_b_ptr,
+    qkv_w_ptr, qkv_b_ptr,
+    proj_w_ptr, proj_b_ptr,
+    rpe_ptr,
+    ln2_w_ptr, ln2_b_ptr,
+    fc1_w_ptr, fc1_b_ptr,
+    fc2_w_ptr, fc2_b_ptr,
+    n_windows,
+    C: tl.constexpr,
+    N: tl.constexpr,
+    HID: tl.constexpr,
+    M: tl.constexpr,
+    BLOCK_W: tl.constexpr,
+    INV_SQRT_C: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    w_start = pid * BLOCK_W
+
+    offs_m = tl.arange(0, M)
+    offs_c = tl.arange(0, C)
+    offs_h = tl.arange(0, HID)
+    offs_n = tl.arange(0, N)
+
+    win_idx_row = offs_m // N + w_start
+    tok_idx_row = offs_m % N
+    row_valid = win_idx_row < n_windows
+
+    # Load X: shape (M, C)
+    x_ptrs = X_ptr + win_idx_row[:, None] * (N * C) + tok_idx_row[:, None] * C + offs_c[None, :]
+    x = tl.load(x_ptrs, mask=row_valid[:, None], other=0.0).to(tl.float32)
+
+    # LN1
+    mean = tl.sum(x, axis=1) / C
+    xm = x - mean[:, None]
+    var = tl.sum(xm * xm, axis=1) / C
+    rstd = 1.0 / tl.sqrt(var + 1e-5)
+    ln1_w = tl.load(ln1_w_ptr + offs_c).to(tl.float32)
+    ln1_b = tl.load(ln1_b_ptr + offs_c).to(tl.float32)
+    x_ln1 = xm * rstd[:, None] * ln1_w[None, :] + ln1_b[None, :]
+    x_ln1_f16 = x_ln1.to(tl.float16)
+
+    # QKV: three (C,C) matmuls
+    qw_ptr = qkv_w_ptr + offs_c[:, None] * (3 * C) + offs_c[None, :]
+    kw_ptr = qkv_w_ptr + offs_c[:, None] * (3 * C) + (C + offs_c[None, :])
+    vw_ptr = qkv_w_ptr + offs_c[:, None] * (3 * C) + (2 * C + offs_c[None, :])
+    qw = tl.load(qw_ptr)
+    kw = tl.load(kw_ptr)
+    vw = tl.load(vw_ptr)
+    qb = tl.load(qkv_b_ptr + offs_c).to(tl.float32)
+    kb = tl.load(qkv_b_ptr + (C + offs_c)).to(tl.float32)
+    vb = tl.load(qkv_b_ptr + (2 * C + offs_c)).to(tl.float32)
+
+    q = tl.dot(x_ln1_f16, qw, out_dtype=tl.float32) + qb[None, :]
+    k = tl.dot(x_ln1_f16, kw, out_dtype=tl.float32) + kb[None, :]
+    v = tl.dot(x_ln1_f16, vw, out_dtype=tl.float32) + vb[None, :]
+
+    # Attention: do it as one big block-diagonal matmul using masks.
+    # q: (M, C), k: (M, C). We want per-window attn (N,N).
+    # Compute full (M, M) score matrix then mask out cross-window entries.
+    # M = BLOCK_W * N, so (M,M) is BLOCK_W*N x BLOCK_W*N = e.g. 128x128. OK.
+    q_f16 = q.to(tl.float16)
+    k_f16 = k.to(tl.float16)
+    v_f16 = v.to(tl.float16)
+
+    k_t = tl.trans(k_f16)  # (C, M)
+    scores = tl.dot(q_f16, k_t, out_dtype=tl.float32) * INV_SQRT_C  # (M, M)
+
+    # Add rpe per-window block: rpe shape (N,N) broadcast across windows.
+    # row window idx: win_idx_row (M,), col window idx: same.
+    win_row = win_idx_row[:, None]
+    win_col = win_idx_row[None, :]
+    same_win = win_row == win_col  # (M, M)
+
+    tok_row = tok_idx_row[:, None]
+    tok_col = tok_idx_row[None, :]
+    rpe_vals = tl.load(rpe_ptr + tok_row * N + tok_col).to(tl.float32)  # (M, M)
+
+    NEG_INF = -1.0e30
+    scores = tl.where(same_win, scores + rpe_vals, NEG_INF)
+
+    # softmax over axis=1
+    smax = tl.max(scores, axis=1)
+    scores = scores - smax[:, None]
+    sexp = tl.exp(scores)
+    ssum = tl.sum(sexp, axis=1)
+    attn = (sexp / ssum[:, None]).to(tl.float16)
+
+    ctx = tl.dot(attn, v_f16, out_dtype=tl.float32)  # (M, C)
+    ctx_f16 = ctx.to(tl.float16)
+
+    # proj: (M,C) @ (C,C)
+    proj_w = tl.load(proj_w_ptr + offs_c[:, None] * C + offs_c[None, :])
+    proj_b = tl.load(proj_b_ptr + offs_c).to(tl.float32)
+    proj_out = tl.dot(ctx_f16, proj_w, out_dtype=tl.float32) + proj_b[None, :]
+
+    x1 = x + proj_out
+
+    # LN2
+    mean2 = tl.sum(x1, axis=1) / C
+    xm2 = x1 - mean2[:, None]
+    var2 = tl.sum(xm2 * xm2, axis=1) / C
+    rstd2 = 1.0 / tl.sqrt(var2 + 1e-5)
+    ln2_w = tl.load(ln2_w_ptr + offs_c).to(tl.float32)
+    ln2_b = tl.load(ln2_b_ptr + offs_c).to(tl.float32)
+    x_ln2 = xm2 * rstd2[:, None] * ln2_w[None, :] + ln2_b[None, :]
+    x_ln2_f16 = x_ln2.to(tl.float16)
+
+    # fc1: (M,C) @ (C,HID)
+    fc1_w = tl.load(fc1_w_ptr + offs_c[:, None] * HID + offs_h[None, :])
+    fc1_b = tl.load(fc1_b_ptr + offs_h).to(tl.float32)
+    h_out = tl.dot(x_ln2_f16, fc1_w, out_dtype=tl.float32) + fc1_b[None, :]
+
+    # GELU tanh
+    k0 = 0.7978845608028654
+    k1 = 0.044715
+    h_g = 0.5 * h_out * (1.0 + tl.extra.libdevice.tanh(k0 * (h_out + k1 * h_out * h_out * h_out)))
+    h_g_f16 = h_g.to(tl.float16)
+
+    # fc2: (M,HID) @ (HID,C)
+    fc2_w = tl.load(fc2_w_ptr + offs_h[:, None] * C + offs_c[None, :])
+    fc2_b = tl.load(fc2_b_ptr + offs_c).to(tl.float32)
+    fc2_out = tl.dot(h_g_f16, fc2_w, out_dtype=tl.float32) + fc2_b[None, :]
+
+    y = (x1 + fc2_out).to(tl.float16)
+
+    y_ptrs = Y_ptr + win_idx_row[:, None] * (N * C) + tok_idx_row[:, None] * C + offs_c[None, :]
+    tl.store(y_ptrs, y, mask=row_valid[:, None])
+
+
+class ModelNew(nn.Module):
+    def __init__(self, dim, num_heads, window_size, shift_size, mlp_ratio=4.0, **_):
+        super().__init__()
+        assert num_heads == 1
+        assert tuple(shift_size) == (0, 0)
+        hidden = int(dim * mlp_ratio)
+        self.dim = dim
+        self.num_heads = num_heads
+        self.window_size = list(window_size)
+        self.hidden = hidden
+
+        self.norm1 = nn.LayerNorm(dim)
+        self.qkv = nn.Linear(dim, 3 * dim, bias=True)
+        self.proj = nn.Linear(dim, dim, bias=True)
+        self.norm2 = nn.LayerNorm(dim)
+        self.fc1 = nn.Linear(dim, hidden, bias=True)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden, dim, bias=True)
+
+        for m in (self.fc1, self.fc2):
+            nn.init.xavier_uniform_(m.weight)
+            nn.init.normal_(m.bias, std=1e-6)
+
+        rpe = _make_relative_position_bias(self.window_size, num_heads, dtype=torch.float32)
+        self.register_buffer("rpe_bias", rpe)
+
+        self.half()
+
+        self._cached = False
+
+    def _prepare_weights(self):
+        if self._cached:
+            return
+        self._qkv_w_t = self.qkv.weight.t().contiguous()
+        self._qkv_b = self.qkv.bias.contiguous()
+        self._proj_w_t = self.proj.weight.t().contiguous()
+        self._proj_b = self.proj.bias.contiguous()
+        self._fc1_w_t = self.fc1.weight.t().contiguous()
+        self._fc1_b = self.fc1.bias.contiguous()
+        self._fc2_w_t = self.fc2.weight.t().contiguous()
+        self._fc2_b = self.fc2.bias.contiguous()
+        self._rpe_f16 = self.rpe_bias.to(torch.float16).contiguous()
+        self._cached = True
+
+    def forward(self, x: Tensor) -> Tensor:
+        self._prepare_weights()
+        B, H, W, C = x.shape
+        Wh, Ww = self.window_size
+        nH, nW = H // Wh, W // Ww
+        N = Wh * Ww
+        hidden = self.hidden
+
+        xw = x.view(B, nH, Wh, nW, Ww, C).permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, N, C)
+        n_windows = xw.shape[0]
+
+        out = torch.empty_like(xw)
+
+        BLOCK_W = 4
+        M = BLOCK_W * N
+        grid = ((n_windows + BLOCK_W - 1) // BLOCK_W,)
+
+        inv_sqrt_c = 1.0 / (C ** 0.5)
+
+        _swin_block_kernel[grid](
+            xw, out,
+            self.norm1.weight, self.norm1.bias,
+            self._qkv_w_t, self._qkv_b,
+            self._proj_w_t, self._proj_b,
+            self._rpe_f16,
+            self.norm2.weight, self.norm2.bias,
+            self._fc1_w_t, self._fc1_b,
+            self._fc2_w_t, self._fc2_b,
+            n_windows,
+            C=C, N=N, HID=hidden, M=M,
+            BLOCK_W=BLOCK_W,
+            INV_SQRT_C=inv_sqrt_c,
+            num_warps=4,
+        )
+
+        y = out.view(B, nH, nW, Wh, Ww, C).permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, C)
+        return y
