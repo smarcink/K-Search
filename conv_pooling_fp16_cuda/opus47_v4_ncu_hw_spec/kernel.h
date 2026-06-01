02fe72d87c3e373f7a8c8dbfa8645f35f8d893bf
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2026-06-01 09:04:14 +0000

    minor cleanup

--- conv_pooling_fp16_cuda/opus47_v4_ncu_hw_spec/kernel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cuda_runtime.h>
-#include <cuda_fp16.h>
-
-void set_conv_params(const void* weight, const void* bias);
-void launch_fused_conv_relu_pool(
-    const __half* input, __half* output,
-    int N, int C, int H, int W, int K,
-    cudaStream_t stream);
